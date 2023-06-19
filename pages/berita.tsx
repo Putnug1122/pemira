00436a35086cd0bcf30cf861b58b9d9263c9ac94
Putra Nugraha <asiapatta10@gmail.com>
2023-06-19 07:51:30 +0700

    new footer for all berita and fixing type error

--- pages/berita.tsx
+++ pages/berita.tsx
@@ -1,91 +1,17 @@
 import { HeaderMegaMenu } from '../components/Header/Header';
-import { FooterLinks } from '../components/Footer/Footer Back';
 import { Box } from '@mantine/core';
 import { BeritaLengkap } from '../components/Berita/BeritaLengkap';
+import { Footer } from '../components/Footer/Footer';
 
-
-const data = [
-    {
-      title: 'About',
-      links: [
-        {
-          label: 'Features',
-          link: '#',
-        },
-        {
-          label: 'Pricing',
-          link: '#',
-        },
-        {
-          label: 'Support',
-          link: '#',
-        },
-        {
-          label: 'Forums',
-          link: '#',
-        },
-      ],
-    },
-    {
-      title: 'Project',
-      links: [
-        {
-          label: 'Contribute',
-          link: '#',
-        },
-        {
-          label: 'Media assets',
-          link: '#',
-        },
-        {
-          label: 'Changelog',
-          link: '#',
-        },
-        {
-          label: 'Releases',
-          link: '#',
-        },
-      ],
-    },
-    {
-      title: 'Community',
-      links: [
-        {
-          label: 'Join Discord',
-          link: '#',
-        },
-        {
-          label: 'Follow on Twitter',
-          link: '#',
-        },
-        {
-          label: 'Email newsletter',
-          link: '#',
-        },
-        {
-          label: 'GitHub discussions',
-          link: '#',
-        },
-      ],
-    },
-];
-
-
-export default function BeritaPemira() {;
-
-  
-    
+export default function BeritaPemira() {
   return (
     <>
       <Box sx={(theme) => ({ position: 'relative', zIndex: 1, boxShadow: theme.shadows.sm })}>
         <HeaderMegaMenu />
         <BeritaLengkap />
       </Box>
-      <FooterLinks data={data} />
+      {/* <FooterLinks data={data} /> */}
+      <Footer />
     </>
   );
-  }
-
-  
-
-
+}
